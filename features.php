17b6f06af4765f0a999ab2b68031eee98602a2ca
Andoni Morales Alastruey <ylatuya@gmail.com>
2011-06-13 17:18:12 +0200

    Fill features content

--- features.php
+++ features.php
@@ -5,12 +5,32 @@
 <div id="content">
   <div id="container">
     <div id="inner-container" class="no-home">
-        <ul>
-        <li>A</li>
-        <li>A</li>        
-        <li>A</li>
+      <h1>LongoMatch, sports video analysys made easy</h1>
+      <p>LongoMatch is a sports video analysis tool for coaches and sports analysts, to assist them in making games video analysis. It simplifies video analysis by providing a set of intuitive tools to tag, review and edit the most important plays of the game. It allows to group plays by categories and adjust the lead and lag time of each play frame by frame through a timeline. It also has support for playlists, an easy way to create presentations with plays from different games and provides a video editor to render new videos with your favourite ones. Even if it is primary focused to sports, LongoMatch can be used for any task that requires tagging and reviewing segments of a video file, and can be applied to fields like cinema, medics or conferences.
+      </p>
+      <h1>Features</h1>
+      <p>
+        <ul id="list">
+          <li>Friendly graphical user interface</li>
+          <li>Design focused on usability and efficiency</li>
+          <li>Unlimited number of tagging categories to group plays</li>
+          <li>Capture from DV camera </li>
+          <li>Live analysis from a DV camera or a fake source</li>
+          <li>Tags plays by categories, sub-categories or players</li>
+          <li>Projects based on templates, customizable for different kind of analysis and sport</li>
+          <li>Specialised video player with frame stepping support and adjustable play rate</li>
+          <li>One-click review</li>
+          <li>Timeline to change visually the lead and lag time of each play with a frame precision</li>
+          <li>Drawing tool</li>
+          <li>Playlists support</li>
+          <li>Video editor to export playlists to new videos in different formats</li>
+          <li>Export plays to png images with a variable frame rate</li>
+          <li>Export projects to CSV files for statistics analysis</li>
+          <li>Support for the most common video formats</li>
+          <li>Multiplatform (Linux, FreeBSD, Windows,...)</li>
         </ul>
       </p>
+    </div>
   </div>
 </div>
 <?php
