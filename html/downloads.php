8f21165f58a09a833b49e77b4f31ef898fcb6bef
Andoni Morales Alastruey <ylatuya@gmail.com>
2011-06-13 19:14:26 +0200

    Add downloads folder

---
+++ html/downloads.php
@@ -0,0 +1,35 @@
+<?php
+$page = 'downloads';
+include "header.php" ?>
+
+<div id="content">
+  <div id="container">
+    <div id="inner-container" class="no-home">
+      <h3>Download LongoMatch for your operating system</h3>
+<ul id="downloads">
+<li id="dn1"> Windows </li>
+<li id="dn1"> Ubuntu </li>
+<li id="dn1"> Debian </li>
+<li id="dn1"> Redhat </li>
+<p>
+<ul id="downloads">
+<li id="dn1"><a
+href="http://ftp.gnome.org/pub/GNOME/binaries/win32/longomatch/LongoMatch-0.16.8-1.exe"><h3>LongoMatch-0.16.8 for Windows</h3></a></li>
+<li id="dn1"><a
+href="http://ftp.gnome.org/pub/GNOME/sources/longomatch/0.16/"><h3>LongoMatch0.16 Sources</h3></a></li>
+</ul>
+<br clear="all">
+</p>
+
+<h3>Ubuntu/Debian ppa</h3>
+<p>
+LongoMatch is part of Ubuntu since Karmic Koala and can be installed with the packages manager.<br>
+Add the following repository if you want to be updated with last versions:<br>
+<b>   sudo add-apt-repository ppa:ylatuya/ppa</b> <br><br>
+We also recommend to use the latest GStreamer binaries using the following repository:<br>
+<b>   sudo add-apt-repository ppa:gstreamer-developers/ppa</b> 
+</p>
+
+</div>
+<?php
+include "footer.php" ?>
