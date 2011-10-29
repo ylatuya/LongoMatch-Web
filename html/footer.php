a134af423e02dc158a63e1e567048b7b7b7b1658
Andoni Morales Alastruey <ylatuya@gmail.com>
2011-10-29 20:29:22 +0200

    Add CC lincense in the footer for the web content

--- html/footer.php
+++ html/footer.php
@@ -1,5 +1,9 @@
     <div id="footer">
-      Copyright 2007-2010 Andoni Morales Alastruey
+      <a rel="license"
+      href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img
+      alt="Licencia de Creative Commons" style="border-width:0"
+      src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a>
+      LongoMatch: The digital coach
     </div>
   </body>
 </html>
