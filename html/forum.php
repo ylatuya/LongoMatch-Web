f66dde952b471f07681688e319da1d14bd143af3
Andoni Morales Alastruey <ylatuya@gmail.com>
2011-10-30 15:30:11 +0100

    Fix forum size

--- html/forum.php
+++ html/forum.php
@@ -4,8 +4,8 @@ include "header.php" ?>
 
     <div id="content">
         <div id="container">
-            <div id="inner-container">
-                <iframe src="forum_index.php/" width=100% height=100%>
+            <div id="inner-container" class="forum">
+                <iframe src="forum_index.php/" width=100% height=100% frameborder="0" scrolling="no">
             </div>
         </div>
     </div>
