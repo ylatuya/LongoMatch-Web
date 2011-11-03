7d4c1598fde08153f55ac7994586ecc4654d100c
ygneo <antonio.barcia@gmail.com>
2011-11-03 23:34:17 +0100

    Some minor style fixes

--- html/contribute.php
+++ html/contribute.php
@@ -5,6 +5,7 @@ include "header.php" ?>
 <div id="content">
   <div id="container">
     <div id="inner-container">
+      <div id="text-content">
       <h2>Contribute to LongoMatch!</h2>
       <h3>Filling Bugs and requesting new features</h3>
       <p>If you find bugs or you want to request a new feature in LongoMatch, you should file a new bug report using GNOME's Bugzilla. You will need to <a href="https://bugzilla.gnome.org/createaccount.cgi">create an account</a> first for filling new bugs or commenting on them.</p>
@@ -63,6 +64,7 @@ include "header.php" ?>
       <p>
 	    If you liked the software and you would like to contribute with a financial help to maintain the web server and invest in new equipment, we accept donations through paypal.</p>
     </div>
+      </div>
   </div>
 </div>
 
--- html/footer.php
+++ html/footer.php
@@ -1,5 +1,5 @@
     <div id="footer">
-      <a rel="license"
+      <a id="cc" rel="license"
       href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img
       alt="Licencia de Creative Commons" style="border-width:0"
       src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a>
