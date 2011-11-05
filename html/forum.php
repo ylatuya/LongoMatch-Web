afa67d1274f2590e477fc75fd5318210ab138fd5
ygneo <antonio.barcia@gmail.com>
2011-11-05 21:01:13 +0100

    Forum correctly integrated and its config.php ignored. Fixed style in features

--- html/forum.php
+++ html/forum.php
@@ -5,10 +5,8 @@ include "header.php" ?>
     <div id="content">
         <div id="container">
             <div id="inner-container" class="forum">
-                <iframe src="forum_index.php/" width=100% height=100% frameborder="0" scrolling="no">
+	        <iframe src="forum/index.php" style="position: relative;"></iframe>
+                <!--<iframe src="forum/index.php" frameborder="0" scrolling="no">-->
             </div>
-        </div>
-    </div>
-
 <?php
 include "footer.php" ?>
