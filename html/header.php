0e420636cee52e2c9c9549403bbc7b54caef8916
Andoni Morales Alastruey <ylatuya@gmail.com>
2011-11-07 00:57:23 +0100

    Add google analytics

--- html/header.php
+++ html/header.php
@@ -24,6 +24,16 @@
     <link rel="stylesheet" type="text/css" media="screen" href="<?php print BASE_URL ?>forum/style/Oxygen/Oxygen_cs.css" />
     <script type="text/javascript" src="<?php print BASE_URL ?>forum/include/js/common.js"></script>
     <!-- End of forum stuff -->
+    <!-- Google analytics -->
+    <script type="text/javascript">
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+    var pageTracker = _gat._getTracker("UA-4991423-1");
+    pageTracker._initData();
+    pageTracker._trackPageview();
+    </script>
     </head>
 </head>
 <body>
